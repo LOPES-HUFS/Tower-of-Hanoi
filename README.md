4c368078baf8a60cc439fdd1d6fac2ad29c3354e
Jaehwan <dialektike@gmail.com>
2021-08-23 17:49:59 +0900

    교재에 있는 내용을 하노이의 탑 프로젝트 시작

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Tower-of-Hanoi
+
 Tower of Hanoi with Rust
+
+## 이 프로젝트 설치 하는 법
+
+우선 본인에 `git`에 이 프로젝트가 설치되어 있다는 것을 가정하겠습니다.
+
+```bash
+❯ ls 
+Tower-of-Hanoi 
+❯ cd Tower-of-Hanoi 
+❯ ls
+Cargo.toml LICENSE    README.md  src
+```
+
+여기서 다음과 같이 `cargo` 를 이용합니다.
+
+```bash
+cargo init Tower-of-Hanoi
+```
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::process;
+
+use tower::Config;
+
+fn main() {
+    //println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!(
+            "입력한 명령어에서 다음과 같은 문제가 발생했습니다: {}",
+            err
+        );
+        process::exit(1);
+    });
+    println!("탑 갯수: {}", config.num_of_tower);
+    println!("대상 파일 이름: {}", config.filename);
+
+    if let Err(e) = tower::run(config) {
+        println!("어플리케이션 에러: {}", e);
+
+        process::exit(1);
+    }
+}
