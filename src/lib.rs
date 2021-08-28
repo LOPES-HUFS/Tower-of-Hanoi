a1532be02198fff7eecd29f71f00ce0176188c40
Jaehwan <dialektike@gmail.com>
2021-08-28 17:13:28 +0900

    인터넷에 있는 소스 코드 추가

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::fmt::{Display, Formatter};
 
 pub struct Config {
     pub num_of_tower: String,
@@ -21,4 +22,62 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = config.num_of_tower;
     println!("\n탑 높이:\n{}", contents);
     Ok(())
+}
+
+// https://gist.github.com/lovasoa/f265350ecd5aa7511235e2d122695725
+
+pub struct Hanoi {
+    disks: [Vec<u32>; 3],
+    size: u32,
+}
+
+impl Hanoi {
+    pub fn new(num_disks: u32) -> Self {
+        Hanoi {
+            size: num_disks,
+            disks: [(1..=num_disks).rev().collect(), vec![], vec![]],
+        }
+    }
+
+    fn move_disk(&mut self, from: usize, to: usize) {
+        let d = self.disks[from].pop().unwrap();
+        self.disks[to].push(d);
+        println!("{}", self);
+    }
+
+    fn solve_at(&mut self, n: u32, from: usize, middle: usize, to: usize) {
+        if n > 0 {
+            self.solve_at(n - 1, from, to, middle);
+            self.move_disk(from, to);
+            self.solve_at(n - 1, middle, from, to);
+        }
+    }
+
+    pub fn solve(&mut self) {
+        println!("{}", self);
+        self.solve_at(self.size, 0, 1, 2);
+    }
+}
+
+impl Display for Hanoi {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        let s = self.size as usize;
+        for i in (0..s).rev() {
+            for j in 0..(3 * s) {
+                if j % s == 0 {
+                    write!(f, "║")?;
+                }
+                write!(
+                    f,
+                    "{}",
+                    match self.disks[j / s].get(i) {
+                        Some(&n) if (n as usize) > (j % s) => '─',
+                        _ => ' ',
+                    }
+                )?;
+            }
+            write!(f, "║\n")?;
+        }
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,18 @@ use std::env;
 use std::process;
 
 use tower::Config;
+use tower::Hanoi;
 
 fn main() {
     //println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!(
-            "입력한 명령어에서 다음과 같은 문제가 발생했습니다: {}",
-            err
-        );
+        println!("입력한 명령어에서 다음과 같은 문제가 발생했습니다: {}", err);
         process::exit(1);
     });
     println!("탑 높이: {}", config.num_of_tower);
+    let num = config.num_of_tower.parse::<u32>().unwrap();
+    Hanoi::new(num).solve();
 
     if let Err(e) = tower::run(config) {
         println!("어플리케이션 에러: {}", e);
